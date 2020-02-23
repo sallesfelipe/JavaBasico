2ead44f09a501c69b2b2d6f45b37427849b92245
Felipe Salles <felipe.salles@outlook.com>
2020-02-22 21:06:15 -0300

    Count days

--- src/IdadeEmDias.java
+++ src/IdadeEmDias.java
@@ -10,12 +10,12 @@ public class IdadeEmDias {
 		int year = scanner.nextInt();
 		
 		System.out.print("Quantos meses voce tem? ");
-		int mounth = scanner.nextInt();
+		int month = scanner.nextInt();
 		
 		System.out.print("Quantos dias voce tem? ");
 		int day = scanner.nextInt();
 		
-		int dateInDays = (year * 365) + (mounth * 30) + day;
+		int dateInDays = (year * 365) + (month * 30) + day;
 		
 		System.out.println("Sua idade em dias é: " +  dateInDays);
 	}
